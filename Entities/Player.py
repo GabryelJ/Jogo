bb652f3220d91eb4574271cb2efd07ce4db348c6
GabryelJ. <gabryeljuliods@gmail.com>
2024-05-28 23:26:32 -0300

    World(Classe Ground, sprite ground.), Program(constants, ground.), Player(movimentos player).

--- Entities/Player.py
+++ Entities/Player.py
@@ -1,20 +1,26 @@
 import pygame
+from Program.constants import SPEED, GAME_SPEED
+
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         pygame.sprite.Sprite.__init__(self)  # inicializa Sprite
-        self.image = pygame.image.load('Entities/sprites/sprite_0.png')
+        self.image = pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha()
         self.rect = pygame.Rect(100, 100, 100, 100)
         self.current_image = 0
         # lista contem sequencia de imagens para animação do boneco correndo.
-        self.sprite_running = [pygame.image.load('Entities/sprites/sprite_0.png'),
-                               pygame.image.load('Entities/sprites/sprite_1.png'),
-                               pygame.image.load('Entities/sprites/sprite_2.png'),
-                               pygame.image.load('Entities/sprites/sprite_3.png')]
+        self.sprite_running = [pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha(),
+                               pygame.image.load('Entities/sprites/sprite_1.png').convert_alpha(),
+                               pygame.image.load('Entities/sprites/sprite_2.png').convert_alpha(),
+                               pygame.image.load('Entities/sprites/sprite_3.png').convert_alpha()]
 
     def update(self, *args):
         def player_movement(self):
-            # key = pygame.key.get_pressed()
+            key = pygame.key.get_pressed()
+            if key[pygame.K_d]:  # se a tecla d for pressionada move para direita
+                self.rect[0] += GAME_SPEED
+            if key[pygame.K_a]:  # se a tecla a for pressionada move para esquerda
+                self.rect[0] -= GAME_SPEED
             self.current_image = (self.current_image + 1) % 4
             self.image = self.sprite_running[self.current_image]
             self.image = pygame.transform.scale(self.image, [100, 100])
--- Program/main.py
+++ Program/main.py
@@ -1,44 +1,46 @@
 import pygame
 from Entities.Player import Player
+from World.Ground import Ground
+from Program.constants import HEIGHT, WIDTH
 
 if __name__ == '__main__':
     pygame.init()
-    height = 800
-    width = 600
-    screen = pygame.display.set_mode((width, height))
-    speed = 4
-    game_speed = 8
-    # TODO: definir plano de fundo?
+    # TODO: definir plano de fundo
     # BACKGROUND = recebe o path para o plano de fundo
+    screen = pygame.display.set_mode((WIDTH, HEIGHT))
 
     clock = pygame.time.Clock()
+
     running = True
 
     playerGroup = pygame.sprite.Group()
     player = Player()
     playerGroup.add(player)
 
+    groundGroup = pygame.sprite.Group()
+    ground = Ground()
+    groundGroup.add(ground)
+
 
     def draw():
         playerGroup.draw(screen)
+        groundGroup.draw(screen)
 
 
     def update():
         playerGroup.update()
+        groundGroup.update()
 
 
     while running:
-        clock.tick(4)  # limita frames por segundo em 4
-
+        clock.tick(4)  # limita frames por segundo em 12
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
 
         update()
-        screen.fill("red")  # limpa rastros do frame anterior cobrindo o frame atual com a cor do parametro até o proximo frame ser produzido.
+        screen.fill("white")  # limpa rastros do frame anterior cobrindo o frame atual com a cor do parametro até o proximo frame ser produzido.
         draw()
-
-        # flip() atualiza o frame.
-        pygame.display.flip()
+        pygame.display.flip()  # flip() atualiza o frame.
 
     pygame.quit()

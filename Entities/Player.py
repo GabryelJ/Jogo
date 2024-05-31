7c6e02c33bbcb54ae2faee2a6e271d27b2cc1ee7
lucasferreiralima <lucasfl2001@gmail.com>
2024-05-31 00:27:24 -0300

    Update Player.py
    Adição da funcionalidade da animação de atirar

--- Entities/Player.py
+++ Entities/Player.py
@@ -1,12 +1,12 @@
 import pygame
 
-
 class Player(pygame.sprite.Sprite):
+
     def __init__(self):
         self.speed = 4  # velocidade do boneco
         self.gravity = 0.3  # gravidade do boneco
         self.vertical_velocity = 0 # Velocidade vertical inicial
-        self.jump_speed = -40  # Velocidade inicial do salto (negativa para subir)
+        self.jump_speed = -20  # Velocidade inicial do salto (negativa para subir)
         self.on_ground = True  # Para verificar se o jogador está no chão
         pygame.sprite.Sprite.__init__(self)  # inicializa Sprite
         # carrega imagem e convert_alpha() mantem os pixels transparentes
@@ -17,7 +17,11 @@ class Player(pygame.sprite.Sprite):
         self.sprite_running = [pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_1.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_2.png').convert_alpha(),
-                               pygame.image.load('Entities/sprites/sprite_3.png').convert_alpha()]
+                               pygame.image.load('Entities/sprites/sprite_3.png').convert_alpha(),
+                               pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha()]
+        self.is_shooting = False
+        self.shooting_timer = 0
+        self.shooting_duration = 10
 
     def player_movement(self):
         key = pygame.key.get_pressed()
@@ -29,6 +33,8 @@ class Player(pygame.sprite.Sprite):
             self.walk()
         if key[pygame.K_SPACE] and self.on_ground:  # Tecla de espaço para saltar
             self.jump()
+        if key[pygame.K_s]:
+            self.shoot()
         #TODO: animação de atirar + tiro
 
 
@@ -54,7 +60,17 @@ class Player(pygame.sprite.Sprite):
             self.vertical_velocity = 0
             self.on_ground = True
 
+
+
+
     def jump(self):
         if self.on_ground:  # Permite pular somente se estiver no chão
             self.vertical_velocity = self.jump_speed
             self.on_ground = False  # Marca que o jogador não está no chão
+
+
+    def shoot(self):
+       #if not self.is_shooting:
+            self.is_shooting = True
+            self.image = self.sprite_running[4]
+            self.shooting_timer = 0

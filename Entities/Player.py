44c5514fa38c23f07f40efcb7ce7d7418a001bb5
lucasferreiralima <lucasfl2001@gmail.com>
2024-05-31 18:25:02 -0300

    Adicionada a Funcionalidade de virar para frente e para trás e atirar para frente e para tras. Definida a cadencia de tiros por segundo e a velocidade da bala ajustada bem como a bala saindo da arma

--- Entities/Player.py
+++ Entities/Player.py
@@ -14,23 +14,28 @@ class Player(pygame.sprite.Sprite):
         self.rect = self.image.get_rect()
         self.current_image = 0
         # lista contem sequencia de imagens para animação do boneco correndo.
-        self.sprite_running = [pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha(),
+        self.sprite_runningDireita = [pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_1.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_2.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_3.png').convert_alpha(),
                                pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha()]
+        #Fazer a mesma cois do runningDireita só que para esquerda
+        self.sprite_runningEsquerda = [pygame.transform.flip(img, True, False) for img in self.sprite_runningDireita]
         self.is_shooting = False
         self.shooting_timer = 0
         self.shooting_duration = 10
         self.bullet_group = bullet_group
         self.contadortiros=5
+        self.andando="direita"
 
     def player_movement(self):
         key = pygame.key.get_pressed()
         if key[pygame.K_d]:  # se a tecla d for pressionada move para direita
+            self.andando = "direita"
             self.rect[0] += self.speed
             self.walk()
         if key[pygame.K_a]:  # se a tecla a for pressionada move para esquerda
+            self.andando="esquerda"
             self.rect[0] -= self.speed
             self.walk()
         if key[pygame.K_SPACE] and self.on_ground:  # Tecla de espaço para saltar
@@ -44,13 +49,19 @@ class Player(pygame.sprite.Sprite):
 
 
         if not any (key):
-            self.image = self.sprite_running[0]
+            if(self.andando=="direita"):
+             self.image = self.sprite_runningDireita[0]
+            else:
+             self.image = self.sprite_runningEsquerda[0]
 
         self.image = pygame.transform.scale(self.image, [100, 98])
 
     def walk(self):
         self.current_image = (self.current_image + 0.2) % 4
-        self.image = self.sprite_running[int(self.current_image)]
+        if(self.andando=="direita"):
+          self.image = self.sprite_runningDireita[int(self.current_image)]
+        else:
+            self.image = self.sprite_runningEsquerda[int(self.current_image)]
 
     def update(self, *args):
 
@@ -77,10 +88,16 @@ class Player(pygame.sprite.Sprite):
     def shoot(self):
        #if not self.is_shooting:
             #self.is_shooting = True
-            self.image = self.sprite_running[4]
+            if(self.andando=="direita"):
+               self.image = self.sprite_runningDireita[4]
+            else:
+               self.image = self.sprite_runningEsquerda[4]
             self.shooting_timer = 0
             self.contadortiros += 1
 
             if(self.contadortiros % 10==0):
-              bullet = Bullet(self.rect.centerx, self.rect.centery, 10)
+              if(self.andando=="direita"):
+                bullet = Bullet(self.rect.centerx, self.rect.centery, 10,"direita")
+              else:
+                bullet = Bullet(self.rect.centerx, self.rect.centery, 10, "esquerda")
               self.bullet_group.add(bullet)
--- Entities/sprites/Bullet.py
+++ Entities/sprites/Bullet.py
@@ -1,14 +1,21 @@
 import pygame
 
 class Bullet(pygame.sprite.Sprite):
-    def __init__(self, x, y, speed):
+    def __init__(self, x, y, speed,direcao):
         super().__init__()
-        self.image = pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha()
-        self.rect = self.image.get_rect()
-        self.rect.center = (x+69, y+20)
-        self.speed = speed
+        if (direcao== "direita"):
+            self.image = pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha()
+            self.rect = self.image.get_rect()
+            self.rect.center = (x+69, y+20)
+            self.speed = speed
+        else:
+            #self.image = pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha()
+            self.image = pygame.transform.flip(pygame.image.load('Entities/sprites/sprite_4.png').convert_alpha(), True,False)
+            self.rect = self.image.get_rect()
+            self.rect.center = (x - 5, y + 20)
+            self.speed = speed * (-1)
 
     def update(self):
         self.rect.x += self.speed  # Mover a bala para a direita
-        if self.rect.x > 800:  # Supondo que a largura da tela seja 800
+        if self.rect.x > 800 or self.rect.x < -100:  # Supondo que a largura da tela seja 800
             self.kill()  # Remover a bala se sair da tela

45382ab3cdaad80f11e67954a948b56fdcc4ece7
sergiolencioni10 <105532428+sergiolencioni10@users.noreply.github.com>
2024-05-30 22:25:24 -0300

    Update Player.py
    Função de pulo do player

--- Entities/Player.py
+++ Entities/Player.py
@@ -4,7 +4,10 @@ import pygame
 class Player(pygame.sprite.Sprite):
     def __init__(self):
         self.speed = 4  # velocidade do boneco
-        self.gravity = 4  # gravidade do boneco
+        self.gravity = 0.3  # gravidade do boneco
+        self.vertical_velocity = 0 # Velocidade vertical inicial
+        self.jump_speed = -40  # Velocidade inicial do salto (negativa para subir)
+        self.on_ground = True  # Para verificar se o jogador está no chão
         pygame.sprite.Sprite.__init__(self)  # inicializa Sprite
         # carrega imagem e convert_alpha() mantem os pixels transparentes
         self.image = pygame.image.load('Entities/sprites/sprite_0.png').convert_alpha()
@@ -24,6 +27,8 @@ class Player(pygame.sprite.Sprite):
         if key[pygame.K_a]:  # se a tecla a for pressionada move para esquerda
             self.rect[0] -= self.speed
             self.walk()
+        if key[pygame.K_SPACE] and self.on_ground:  # Tecla de espaço para saltar
+            self.jump()
         #TODO: animação de atirar + tiro
 
 
@@ -39,5 +44,17 @@ class Player(pygame.sprite.Sprite):
     def update(self, *args):
 
         self.player_movement()
+        # Aplicar gravidade
+        self.vertical_velocity += self.gravity
+        self.rect.y += self.vertical_velocity
 
-        self.rect[1] += self.gravity #
+        # Simular o chão (ajustar conforme necessário)
+        if self.rect.bottom >= 525:  # Suponha que 300 seja a posição do chão
+            self.rect.bottom = 525
+            self.vertical_velocity = 0
+            self.on_ground = True
+
+    def jump(self):
+        if self.on_ground:  # Permite pular somente se estiver no chão
+            self.vertical_velocity = self.jump_speed
+            self.on_ground = False  # Marca que o jogador não está no chão

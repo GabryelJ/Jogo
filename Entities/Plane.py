7737a7550090098ddf2e086480d203a3072b4b5c
GabryelJ. <gabryeljuliods@gmail.com>
2024-06-19 18:52:38 -0300

    Adicionado 'Avião' com entregas de 'munição' e + Ajustes para comportar alteração
    1. Classe Plane:
    avião 'patrulha' o mapa e solta caixas de munição.
    
    2. Classe AmmoBox:
    Gravidade e arte para AmmoBox, a caixa agora é 'destruida' após coletada.
    
    3. main:
    A: verificação de colisão entre os elementos do grupo platform com elementos do grupo AmmoBox para impor "gravidade" em AmmoBox
    
    B: verificação de colisão entre cada player com cada ammo box para reabastecimento
    
    C: remoção da ammobox "default"
    
    D: plane_group criado.

---
+++ Entities/Plane.py
@@ -0,0 +1,35 @@
+import pygame
+from entities.AmmoBox import AmmoBox
+
+
+class Plane(pygame.sprite.Sprite):
+    def __init__(self, ammo_group):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.Surface((100, 50))
+        self.image.fill((192, 192, 192))
+        self.rect = self.image.get_rect()
+        self.speed = 2
+        self.rect.x = 1
+        self.rect.y = 100
+        self.drop_counter = 0
+        self.ammo_group = ammo_group
+
+    def drop(self, drop_counter):
+        self.drop_counter = drop_counter
+        if self.drop_counter % 360 == 0:
+            ammo_drop = AmmoBox(self.rect.x, self.rect.y)
+            self.drop_counter += 1
+            self.ammo_group.add(ammo_drop)
+        self.drop_counter += 1
+    def patrol(self):
+        self.rect.x += self.speed
+        if self.rect.centerx >= 800 or self.rect.centerx <= 0:
+            self.speed *= -1
+
+    def update(self, *args):
+        self.patrol()
+        self.drop(self.drop_counter)
+
+    def draw(self, screen):
+        screen.blit(self.image, self.rect)
+        pygame.draw.rect(screen, (192, 192, 192), self.rect.center)

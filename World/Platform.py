f66f747bc9839c8c1b9e9470e86e96dfc249d020
lucasferreiralima <lucasfl2001@gmail.com>
2024-06-20 00:09:33 -0300

    Grupo todo: Criação do avião e do drop de munição. Correção da hitbox criando uma nova hitbox desvinculada com o rect da entidade. Correção de colisão com plataformas. retirada do chão e mudança de lógica de gravidade

--- World/Platform.py
+++ World/Platform.py
@@ -1,10 +1,18 @@
 import pygame
 
 class Platform(pygame.sprite.Sprite):
-    def __init__(self, x, y, width, height):
+    def __init__(self, x, y, width, height,speedp):
         pygame.sprite.Sprite.__init__(self)
         self.image = pygame.Surface([width, height])
         self.image.fill((139, 69, 19))  # Cor marrom para a plataforma
         self.rect = self.image.get_rect()
         self.rect.x = x
         self.rect.y = y
+        self.speed = speedp
+
+
+    def update(self):
+
+        self.rect.x += self.speed
+        if self.rect.centerx >= 800 or self.rect.centerx <= 0:
+              self.speed *= -1

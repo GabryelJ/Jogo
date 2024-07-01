3989ac5b3e9112b2c3dffc4ef4ef958bec6b2b28
GabryelJ. <gabryeljuliods@gmail.com>
2024-07-01 14:41:33 -0300

    Classe Game
    Código da main organizado em classe Game, preparação para implementar menu.

--- main.py
+++ main.py
@@ -1,114 +1,136 @@
 import pygame
+import constants
+from entities.Plane import Plane
 from entities.Player import Player
 from world.Platform import Platform
-from constants import HEIGHT, WIDTH
-from entities.Plane import Plane
 
 
-def menu():
-    pass
+class Game:
+    def __init__(self):  # inicializa Game
+        pygame.init()
+        self.SCREEN = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+        self.clock = pygame.time.Clock()
+        self.running = True
+        self.game_over = False
+
+    def new_game(self):  # instancia e inicializa grupos e seus componentes
+        self.bullet_group = pygame.sprite.Group()
+        self.player_group = pygame.sprite.Group()
+        self.player1 = Player(self.bullet_group, 1)
+        self.player2 = Player(self.bullet_group, 2)
+        self.player_group.add(self.player1)
+        self.player_group.add(self.player2)
+        self.platform_group = pygame.sprite.Group()
+        self.platform_group.add(Platform(200, 400, 200, 20, 1))
+        self.platform_group.add(Platform(400, 300, 200, 20, 2))
+        self.platform_group.add(Platform(0, 580, 800, 20, 0))
+        self.ammo_group = pygame.sprite.Group()
+        self.plane_group = pygame.sprite.Group()
+        self.plane = Plane(self.ammo_group)
+        self.plane_group.add(self.plane)
+        self.run()
+
+    def draw(self):
+        self.SCREEN.fill("white")
+        self.player_group.draw(self.SCREEN)
+        for player in self.player_group:  # para debug de hitbox
+            player.draw(self.SCREEN)
+
+        for bullet in self.bullet_group: # para debug de hitbox
+            bullet.draw(self.SCREEN)
+
+        self.platform_group.draw(self.SCREEN)
+        self.bullet_group.draw(self.SCREEN)
+        self.ammo_group.draw(self.SCREEN)
+        self.draw_hud()
+        self.plane_group.draw(self.SCREEN)
+
+    def update(self):
+        self.player_group.update()
+        self.bullet_group.update()
+        self.ammo_group.update()
+        self.plane_group.update()
+        self.platform_group.update()
+
+    def draw_hud(self):
+        font = pygame.font.Font(None, 36)
+        player1_text = font.render(f'Player 1 - Health: {self.player1.health} Ammo: {self.player1.ammunition} ', True,
+                                   (0, 0, 0))
+        player2_text = font.render(f'Player 2 - Health: {self.player2.health} Ammo: {self.player2.ammunition}', True,
+                                   (0, 0, 0))
+        self.SCREEN.blit(player1_text, (20, 20))
+        self.SCREEN.blit(player2_text, (20, 60))
+
+    def run(self):  # game loop
+        self.playing = True
+        while self.playing:
+            self.clock.tick(60)
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    if self.playing:
+                        self.playing = False
+                    self.running = False
+
+            self.players_collision()
+            self.check_ammo_status()
+            self.check_bullet_impact()
+            if self.player1.health == 0 or self.player2.health == 0:  # game over
+                self.collect()
+                self.new_game()
+
+            self.update()
+            self.draw()
+            pygame.display.flip()
+
+        pygame.quit()
+
+    def players_collision(self):
+        for player in self.player_group:
+            collisions = pygame.sprite.spritecollide(player, self.platform_group, False)
+            player.on_ground = False
+            for platform in collisions:
+                if player.vertical_velocity >= 0 and player.hitbox.bottom >= platform.rect.top:  # + player.vertical_velocity:
+                    player.hitbox.bottom = platform.rect.top
+                    player.vertical_velocity = 0
+                    player.on_ground = True
+
+    def check_bullet_impact(self):
+        for bullet in self.bullet_group:
+            if pygame.sprite.spritecollide(bullet, self.player_group, False):
+                for player in self.player_group:
+                    if bullet.hitbox.colliderect(player.hitbox):
+                        player.take_damage(bullet.damage)
+                        bullet.kill()
+
+    def check_ammo_status(self):
+        for ammo_box in self.ammo_group:
+            if pygame.sprite.spritecollide(ammo_box, self.platform_group, False):
+                ammo_box.on_ground = True
+                ammo_box.gravity = 0
+            else:
+                ammo_box.on_ground = False
+                ammo_box.gravity = 1
+            for player in self.player_group:
+                if pygame.sprite.collide_rect(ammo_box, player):
+                    player.ammunition = 10
+                    ammo_box.kill()
+
+    def collect(self):  # tratar vazamento de memória após reinicio
+        self.player_group.empty()
+        self.bullet_group.empty()
+        self.ammo_group.empty()
+        self.plane_group.empty()
+        self.platform_group.empty()
+
+    def menu(self): # TODO: 1: um game loop próprio? - 2: interação por teclas
+        # display
+        print('estado menu')
+        pass
 
 
-def players_collision():
-    for player in player_group:
-        collisions = pygame.sprite.spritecollide(player, platform_group, False)
-        player.on_ground = False
-        for platform in collisions:
-            if player.vertical_velocity >= 0 and player.hitbox.bottom >= platform.rect.top:  # + player.vertical_velocity:
-                player.hitbox.bottom = platform.rect.top
-                player.vertical_velocity = 0
-                player.on_ground = True
-
-
-def check_bullet_impact():
-    for bullet in bullet_group:
-        if pygame.sprite.spritecollide(bullet, player_group, False):
-            for player in player_group:
-                if bullet.hitbox.colliderect(player.hitbox):
-                    player.take_damage(bullet.damage)
-                    bullet.kill()
-
-
-def check_ammo_status():
-    for ammo_box in ammo_group:
-        if pygame.sprite.spritecollide(ammo_box, platform_group, False):
-            ammo_box.on_ground = True
-            ammo_box.gravity = 0
-        else:
-            ammo_box.on_ground = False
-            ammo_box.gravity = 1
-        for player in player_group:
-            if pygame.sprite.collide_rect(ammo_box, player):
-                player.ammunition = 10
-                ammo_box.kill()
-
-
-def draw():
-    player_group.draw(SCREEN)
-    for player in player_group:
-        player.draw(SCREEN)
-
-    for bullet in bullet_group:
-        bullet.draw(SCREEN)
-
-    platform_group.draw(SCREEN)
-    bullet_group.draw(SCREEN)
-    ammo_group.draw(SCREEN)
-    draw_hud()
-    plane_group.draw(SCREEN)
-
-
-def update():
-    player_group.update()
-    bullet_group.update()
-    ammo_group.update()
-    plane_group.update()
-    platform_group.update()
-
-
-def draw_hud():
-    font = pygame.font.Font(None, 36)
-    player1_text = font.render(f'Player 1 - Health: {player1.health} Ammo: {player1.ammunition} ', True, (0, 0, 0))
-    player2_text = font.render(f'Player 2 - Health: {player2.health} Ammo: {player2.ammunition}', True, (0, 0, 0))
-    SCREEN.blit(player1_text, (20, 20))
-    SCREEN.blit(player2_text, (20, 60))
-
-
-pygame.init()
-SCREEN = pygame.display.set_mode((WIDTH, HEIGHT))
-clock = pygame.time.Clock()
-
-bullet_group = pygame.sprite.Group()
-player_group = pygame.sprite.Group()
-player1 = Player(bullet_group, 1)
-player2 = Player(bullet_group, 2)
-player_group.add(player1)
-player_group.add(player2)
-platform_group = pygame.sprite.Group()
-
-platform_group.add(Platform(200, 400, 200, 20, 1))
-platform_group.add(Platform(400, 300, 200, 20, 2))
-platform_group.add(Platform(0, 580, 800, 20, 0))
-
-ammo_group = pygame.sprite.Group()
-plane_group = pygame.sprite.Group()
-plane = Plane(ammo_group)
-plane_group.add(plane)
-
-running = True
-while running:
-    clock.tick(60)
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-
-    players_collision()
-    check_ammo_status()
-    check_bullet_impact()
-
-    update()
-    SCREEN.fill("white")
-    draw()
-    pygame.display.flip()
-
-pygame.quit()
+game = Game()
+game.menu()
+while game.running:
+    game.new_game() #loop aqui
+    game.menu()

e73a1314c24085d8256fa6ef83c8cd17114eb1f4
GabryelJ. <gabryeljuliods@gmail.com>
2024-07-01 20:29:36 -0300

    Menu criado, transição de estado entre menu e jogo, tratamento de vazamento de memória.
    1- O menu foi elaborado com seu próprio loop, a partir do menu podemos alterar para o estado do jogo, após a 'morte' de um dos jogadores o estado volta pra o menu.
    OBS - 1.a) Ambos os loops de game e menu são encerrados na transição.
    
    Problemas - estava ocorrendo um vazamento de memória após cada partida, foi criado o método collect() para lidar com isso, o método esvazia os grupos de sprites utilizados.
    OBS - 2.a) Ainda ocorre vazamento, será investigado.
    
    3- screen não é constante, por convenção da linguagem foi passada para lower case.

--- main.py
+++ main.py
@@ -8,10 +8,9 @@ from world.Platform import Platform
 class Game:
     def __init__(self):  # inicializa Game
         pygame.init()
-        self.SCREEN = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
+        self.screen = pygame.display.set_mode((constants.WIDTH, constants.HEIGHT))
         self.clock = pygame.time.Clock()
         self.running = True
-        self.game_over = False
 
     def new_game(self):  # instancia e inicializa grupos e seus componentes
         self.bullet_group = pygame.sprite.Group()
@@ -31,19 +30,19 @@ class Game:
         self.run()
 
     def draw(self):
-        self.SCREEN.fill("white")
-        self.player_group.draw(self.SCREEN)
+        self.screen.fill("white")
+        self.player_group.draw(self.screen)
         for player in self.player_group:  # para debug de hitbox
-            player.draw(self.SCREEN)
+            player.draw(self.screen)
 
-        for bullet in self.bullet_group: # para debug de hitbox
-            bullet.draw(self.SCREEN)
+        for bullet in self.bullet_group:  # para debug de hitbox
+            bullet.draw(self.screen)
 
-        self.platform_group.draw(self.SCREEN)
-        self.bullet_group.draw(self.SCREEN)
-        self.ammo_group.draw(self.SCREEN)
+        self.platform_group.draw(self.screen)
+        self.bullet_group.draw(self.screen)
+        self.ammo_group.draw(self.screen)
         self.draw_hud()
-        self.plane_group.draw(self.SCREEN)
+        self.plane_group.draw(self.screen)
 
     def update(self):
         self.player_group.update()
@@ -58,8 +57,8 @@ class Game:
                                    (0, 0, 0))
         player2_text = font.render(f'Player 2 - Health: {self.player2.health} Ammo: {self.player2.ammunition}', True,
                                    (0, 0, 0))
-        self.SCREEN.blit(player1_text, (20, 20))
-        self.SCREEN.blit(player2_text, (20, 60))
+        self.screen.blit(player1_text, (20, 20))
+        self.screen.blit(player2_text, (20, 60))
 
     def run(self):  # game loop
         self.playing = True
@@ -68,22 +67,21 @@ class Game:
 
             for event in pygame.event.get():
                 if event.type == pygame.QUIT:
-                    if self.playing:
-                        self.playing = False
+                    self.playing = False
                     self.running = False
 
             self.players_collision()
             self.check_ammo_status()
             self.check_bullet_impact()
+
             if self.player1.health == 0 or self.player2.health == 0:  # game over
                 self.collect()
-                self.new_game()
+                self.playing = False
+                self.menu()
 
             self.update()
             self.draw()
             pygame.display.flip()
-
-        pygame.quit()
 
     def players_collision(self):
         for player in self.player_group:
@@ -123,14 +121,42 @@ class Game:
         self.plane_group.empty()
         self.platform_group.empty()
 
-    def menu(self): # TODO: 1: um game loop próprio? - 2: interação por teclas
-        # display
-        print('estado menu')
-        pass
+    def menu(self):
+        font = pygame.font.Font(None, 100)
+        play_text = font.render('Play (1)', True, (0, 0, 0))
+        play_rect = play_text.get_rect(center=(constants.WIDTH / 2, constants.HEIGHT / 2 - 50))
+        quit_text = font.render('Quit (2)', True, (0, 0, 0))
+        quit_rect = quit_text.get_rect(center=(constants.WIDTH / 2, constants.HEIGHT / 2 + 50))
+
+        menu_running = True
+        while menu_running:
+            self.screen.fill("white")
+            self.screen.blit(play_text, play_rect)
+            self.screen.blit(quit_text, quit_rect)
+            pygame.display.flip()
+
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    self.running = False
+                    menu_running = False
+
+                if event.type == pygame.MOUSEBUTTONDOWN:  # interalçao com menu via mouse
+                    if play_rect.collidepoint(event.pos):
+                        menu_running = False
+                        self.new_game()
+                    if quit_rect.collidepoint(event.pos):
+                        self.running = False
+                        menu_running = False
+
+                if event.type == pygame.KEYDOWN:  # interação com menu via teclado
+                    if event.key == pygame.K_1:
+                        menu_running = False
+                        self.new_game()
+                    if event.key == pygame.K_2:
+                        self.running = False
+                        menu_running = False
 
 
 game = Game()
 game.menu()
-while game.running:
-    game.new_game() #loop aqui
-    game.menu()
+pygame.quit()
